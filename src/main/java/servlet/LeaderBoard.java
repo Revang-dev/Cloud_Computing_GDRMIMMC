e5c781961ee25af3f1bb81c976f032564da84669
Ivan Picard Marchetto <ivan.picardmarchetto@gmail.com>
2018-11-15 15:40:12 +0100

    leaderboard part 2

--- src/main/java/servlet/LeaderBoard.java
+++ src/main/java/servlet/LeaderBoard.java
@@ -9,6 +9,7 @@ import Database.UserDataStore;
 import Entity.Users;
 
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.List;
 
 public class LeaderBoard extends HttpServlet {
@@ -20,7 +21,16 @@ public class LeaderBoard extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
     	UserDataStore dataStore = UserDataStore.getInstance();
-    	List<Users> users;
+    	List<Users> users = dataStore.getAllUser();
+    	users.sort(new Comparator<Users>() {
+
+			@Override
+			public int compare(Users o1, Users o2) {
+				return -Integer.compare(o1.getPoint(), o2.getPoint());
+			}
+		});
+    	users = users.subList(0, 9);
+    	
     }
 
     @Override
