890596cb30e12a85e7d39014d02a4de74e38a1ed
Duminy Gaétan <gaetan.duminy@gmail.com>
2018-11-15 15:39:27 +0100

    ARRETER DE MERGE DESSUS SVP

--- src/main/java/utils/JsonGenerator.java
+++ src/main/java/utils/JsonGenerator.java
@@ -1,32 +1,44 @@
-package utils;
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.google.appengine.repackaged.com.google.gson.JsonObject;
+import com.google.cloud.datastore.*;
 
-public class JsonGenerator {
-    static private JsonGenerator instance;
+import Database.UserDataStore;
+import Entity.Users;
+import utils.JsonGenerator;
 
-    private JsonGenerator() {}
+import java.io.IOException;
 
-    static public JsonGenerator getInstance() {
-        if (instance == null) {
-            instance = new JsonGenerator();
+public class Connexion extends HttpServlet {
+    //////////////////////__JSON__///////////////////////////////
+    /*
+    {Action: “Connexion”,
+     Body: {
+        userID: Bob@gmail.com
+        pass: "mot_de_passe"
         }
-        return instance;
     }
+    */
+    /////////////////////////////////////////////////////////////
 
-    static public JsonObject getConnexionJson(String userID, String pass) {
-    	JsonObject json_body = new JsonObject();
-    	JsonObject json = new JsonObject();
-        json_body.addProperty("userID", userID);
-        json_body.addProperty("pass", pass);
-        json.addProperty("Action", "Connexion");
-        json.add("Body", json_body);
-        return json;
-    }
-
-    public static void main (String[] args) {
-    	getInstance();
-    	JsonObject test = getConnexionJson("Bob@gmail.com", "mdp");
-    	System.out.println(test.toString());
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+    	JsonGenerator.getInstance();
+    	UserDataStore.getInstance();
+    	JsonObject json = JsonGenerator.requestToJson(req);
+    	Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    	Users user = null;
+    		
+    	if (json.get("Action").toString().equals("Connexion")) {
+    		JsonObject body = (JsonObject) json.get("Body");
+    		String email = body.get("userID").toString();
+    		String pass = body.get("pass").toString();
+    		user = UserDataStore.getUser(email, pass);
+    	}
     }
 }
