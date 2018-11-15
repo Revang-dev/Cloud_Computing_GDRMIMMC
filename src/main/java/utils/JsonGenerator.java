94658e08b311d2c1be3122ace62584cf76d98979
MEERSMAN Rudy <33250028+Rmeersman@users.noreply.github.com>
2018-11-15 15:46:28 +0100

    Stable Version
    Stable version

--- src/main/java/utils/JsonGenerator.java
+++ src/main/java/utils/JsonGenerator.java
@@ -1,44 +1,43 @@
-package servlet;
+package utils;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.google.appengine.repackaged.com.google.gson.JsonObject;
-import com.google.cloud.datastore.*;
-
-import Database.UserDataStore;
-import Entity.Users;
-import utils.JsonGenerator;
-
+import java.io.BufferedReader;
 import java.io.IOException;
 
-public class Connexion extends HttpServlet {
-    //////////////////////__JSON__///////////////////////////////
-    /*
-    {Action: “Connexion”,
-     Body: {
-        userID: Bob@gmail.com
-        pass: "mot_de_passe"
-        }
-    }
-    */
-    /////////////////////////////////////////////////////////////
+import javax.servlet.http.HttpServletRequest;
 
-    @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-    	JsonGenerator.getInstance();
-    	UserDataStore.getInstance();
-    	JsonObject json = JsonGenerator.requestToJson(req);
-    	Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-    	Users user = null;
-    		
-    	if (json.get("Action").toString().equals("Connexion")) {
-    		JsonObject body = (JsonObject) json.get("Body");
-    		String email = body.get("userID").toString();
-    		String pass = body.get("pass").toString();
-    		user = UserDataStore.getUser(email, pass);
-    	}
+import com.google.appengine.repackaged.com.google.gson.JsonParser;
+import com.google.appengine.repackaged.com.google.gson.JsonElement;
+import com.google.appengine.repackaged.com.google.gson.JsonObject;
+
+public class JsonGenerator {
+    static private JsonGenerator instance;
+
+    private JsonGenerator() {}
+
+    static public JsonGenerator getInstance() {
+        if (instance == null) {
+            instance = new JsonGenerator();
+        }
+        return instance;
+    }
+
+    static public JsonObject requestToJson(HttpServletRequest req) throws IOException {
+        StringBuffer buff = new StringBuffer();
+        String line = null;
+        
+        try {
+            BufferedReader reader = req.getReader();
+            while ((line = reader.readLine()) != null) {
+            	buff.append(line);
+            }
+        } catch (Exception e) { /*report an error*/ }
+
+        try {
+            JsonParser jparser = new JsonParser();
+            JsonObject json = ((JsonElement) jparser.parse(buff.toString())).getAsJsonObject();
+            return json;
+        } catch (Exception e) { /*report an error*/ }
+        
+		return null;
     }
 }
