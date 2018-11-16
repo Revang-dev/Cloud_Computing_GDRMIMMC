6ccbe32cca5be6c45f3ec5831d5d838920227aa0
Ivan Picard Marchetto <ivan.picardmarchetto@gmail.com>
2018-11-16 12:53:30 +0100

    pull queue handler

---
+++ src/main/java/queues/PullQueue.java
@@ -0,0 +1,62 @@
+package queues;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskOptions;
+import com.google.appengine.api.taskqueue.TaskHandle;
+
+public class PullQueue {
+	
+	private static PullQueue instance;
+	
+	public static PullQueue getInstance() {
+		if(instance==null)
+			instance = new PullQueue();
+		return instance;
+	}
+	
+	private Queue myQueue;
+	
+	private PullQueue() {
+		myQueue = QueueFactory.getQueue("pull-queue");
+	}
+	
+	public void addTask(String payload) {
+		myQueue.add(TaskOptions.Builder.withMethod(TaskOptions.Method.PULL).payload(payload));
+	}
+	
+	public void addTask(byte[] payload) {
+		myQueue.add(TaskOptions.Builder.withMethod(TaskOptions.Method.PULL).payload(payload));
+	}
+	
+	public void addTask(byte[] payload,String contentType) {
+		myQueue.add(TaskOptions.Builder.withMethod(TaskOptions.Method.PULL).payload(payload,contentType));
+	}
+	
+	public List<TaskHandle> taskLease(long time, TimeUnit timeUnit,long numberOfLease){
+		return myQueue.leaseTasks(time, timeUnit, numberOfLease);
+	}
+	
+	public void deleteTask(TaskHandle arg0) {
+		myQueue.deleteTask(arg0);
+	}
+	public void deleteTask(String arg0) {
+		myQueue.deleteTask(arg0);
+	}
+	public void deleteTask(List<TaskHandle> arg0) {
+		myQueue.deleteTask(arg0);
+	}
+	public void deleteTaskAsync(TaskHandle arg0) {
+		myQueue.deleteTaskAsync(arg0);
+	}
+	public void deleteTaskAsync(String arg0) {
+		myQueue.deleteTaskAsync(arg0);
+	}
+	public void deleteTaskAsync(List<TaskHandle> arg0) {
+		myQueue.deleteTaskAsync(arg0);
+	}
+
+}
