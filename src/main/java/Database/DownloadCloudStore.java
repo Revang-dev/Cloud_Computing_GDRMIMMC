77e983e9ab0fe340735c1c722154fa6a7fa16f15
Ivan Picard Marchetto <ivan.picardmarchetto@gmail.com>
2018-11-17 14:58:30 +0100

    download cloud store

--- src/main/java/Database/DownloadCloudStore.java
+++ src/main/java/Database/DownloadCloudStore.java
@@ -1,5 +1,11 @@
 package Database;
 
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.Storage.BlobSourceOption;
+import com.google.cloud.storage.StorageOptions;
+import com.google.cloud.storage.BlobId;
+
 public class DownloadCloudStore {
     //////////////////////__JSON__///////////////////////////////
     /*
@@ -11,4 +17,20 @@ public class DownloadCloudStore {
     }
     */
     /////////////////////////////////////////////////////////////
+	
+    private static Storage storage = null;
+    private String bucket = "brave-sonar-218511.appspot.com/";
+
+    static {
+        storage = StorageOptions.getDefaultInstance().getService();
+    }
+    
+    public byte[] downloadFile(String blobName){
+
+        BlobInfo blobInfo = storage.get(BlobId.of(bucket, blobName));
+        
+        byte[] file=storage.readAllBytes(blobInfo.getBlobId(),BlobSourceOption.generationMatch());
+        
+        return file;
+    }
 }
